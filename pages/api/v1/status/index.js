689abdcd4e48274dad521861199e62f48ce4919b
okborges <rafael.pb@partner.samsung.com>
2026-01-27 13:25:06 -0300

    aula final dia 17

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,8 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1+1;");
+  console.log(result.rows);
+  response.status(200).json({ status: "ok" });
+}
+export default status;
