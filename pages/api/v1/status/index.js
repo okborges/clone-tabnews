b76cd66a093f6ba39b2c0ee12e8966fe3b2d73ce
Rafael Borges <rafael.okborges@gmail.com>
2026-02-02 14:06:47 -0300

    rota status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,5 @@
-import database from "/infra/database";
-
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+  response.status(200).json({ update_at: updateAt });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("Get to /api/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const respondeBody = await response.json();
+
+  const parcedUpdateAt = new Date(respondeBody.update_at).toISOString();
+
+  expect(respondeBody.update_at).toBeDefined();
+  expect(respondeBody.update_at).toBe(parcedUpdateAt);
 });
