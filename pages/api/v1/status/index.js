035172ecf41d85b9955d5dd336b1d254fdafde66
okborges <rafael.pb@partner.samsung.com>
2026-02-03 13:44:02 -0300

    finaliza o endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,34 @@
+import database from "infra/database";
+
 async function status(request, response) {
   const updateAt = new Date().toISOString();
-  response.status(200).json({ update_at: updateAt });
+
+  const databaseVersionResult = await database.query("Show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const databaseOpedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpedConnectionsValue,
+      },
+    },
+  });
 }
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,6 +5,8 @@ test("Get to /api/status should return 200", async () => {
 
   const parcedUpdateAt = new Date(respondeBody.update_at).toISOString();
 
-  expect(respondeBody.update_at).toBeDefined();
   expect(respondeBody.update_at).toBe(parcedUpdateAt);
+  expect(respondeBody.dependencies.database.version).toEqual("16.0");
+  expect(respondeBody.dependencies.database.max_connections).toEqual(100);
+  expect(respondeBody.dependencies.database.open_connections).toEqual(1);
 });
