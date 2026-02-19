43cb5d0aefd90913c7fe00f9517f0c0ffb51de54
okborges <rafael.pb@partner.samsung.com>
2026-02-19 14:01:28 -0300

    live run inicio

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,13 +2,29 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  const migration = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migration);
+  if (request.method === "GET") {
+    const migration = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migration);
+  }
+
+  if (request.method === "POST") {
+    const migration = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    return response.status(200).json(migration);
+  }
+
+  return response.status(405).json({ message: "Method not allowed" });
 }
