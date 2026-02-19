4b9965b5e9bfa2b598ccdcdd10e3638ef49c7a1a
okborges <rafael.pb@partner.samsung.com>
2026-02-19 17:33:46 -0300

    migratin refatorada junto com o dotenv

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,5 +1,11 @@
 import database from "infra/database.js";
 
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("Get to /api/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
