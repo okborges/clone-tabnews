f6a48bba844b1e068682663830916114a4ef4004
okborges <rafael.okborges@gmail.com>
2026-05-30 20:57:43 -0300

    fix: updated

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,9 +11,9 @@ describe("GET to /api/status", () => {
       expect(response.status).toBe(200);
       const respondeBody = await response.json();
 
-      const parcedUpdateAt = new Date(respondeBody.update_at).toISOString();
+      const parcedUpdateAt = new Date(respondeBody.updated_at).toISOString();
 
-      expect(respondeBody.update_at).toBe(parcedUpdateAt);
+      expect(respondeBody.updated_at).toBe(parcedUpdateAt);
       expect(respondeBody.dependencies.database.version).toEqual("16.0");
       expect(respondeBody.dependencies.database.max_connections).toEqual(100);
       expect(respondeBody.dependencies.database.open_connections).toEqual(1);
