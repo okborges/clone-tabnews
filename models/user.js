2b56a3f0f62a5d55bb3b5694b65698a52101d75b
okborges <rafael.okborges@gmail.com>
2026-07-03 17:55:59 -0300

    feat: model user

---
+++ models/user.js
@@ -0,0 +1,27 @@
+import database from "infra/database";
+async function create(userInputValues) {
+  const result = await database.query({
+    text: `
+    INSERT INTO 
+      users (username, email,password) 
+    VALUES 
+      ($1, $2, $3)
+      RETURNING
+        *
+    ;`,
+
+    values: [
+      userInputValues.username,
+      userInputValues.email,
+      userInputValues.password,
+    ],
+  });
+
+  return result.rows[0];
+}
+
+const user = {
+  create,
+};
+
+export default user;
