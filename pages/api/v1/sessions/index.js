5a0401015d98299311837c8a6c2791018a116574
okborges <rafael.pb@samsung.com>
2026-08-21 17:30:07 -0300

    feat: create `sessions` migrations and model

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 import authentication from "models/authentication.js";
+import session from "models/session.js";
 
 const router = createRouter();
 
@@ -16,5 +17,7 @@ async function postHandler(request, response) {
     userInputValues.password,
   );
 
-  return response.status(201).json({});
+  const newSession = await session.create(authenticatedUser.id);
+
+  return response.status(201).json(newSession);
 }
