3c042e5f35b7545a510ca84905f496a38bca4a6c
okborges <rafael.pb@samsung.com>
2026-09-11 17:08:47 -0300

    feat: renova sessão

--- models/session.js
+++ models/session.js
@@ -59,8 +59,34 @@ async function create(userId) {
   }
 }
 
+async function renew(sessionId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLSECONDS);
+  const renewedSessionObject = runUpdateQuery(sessionId, expiresAt);
+  return renewedSessionObject;
+
+  async function runUpdateQuery(sessionId, expiresAt) {
+    const results = await database.query({
+      text: `
+        UPDATE
+          sessions
+        SET
+          expires_at = $2,
+          updated_at = NOW()
+        WHERE
+          id = $1
+        RETURNING
+          *
+        ;`,
+      values: [sessionId, expiresAt],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
+  renew,
   findOneValidByToken,
   EXPIRATION_IN_MILLSECONDS,
 };
--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -1,6 +1,7 @@
 import { version as uuidVersion } from "uuid";
+import setCookieParser from "set-cookie-parser";
 import orchestrator from "tests/orchestrator.js";
-import session from "models/session";
+import session from "models/session.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -11,9 +12,7 @@ beforeAll(async () => {
 describe("GET /api/v1/user", () => {
   describe("Default user", () => {
     test("With valid session", async () => {
-      const createdUser = await orchestrator.createUser({
-        username: "UserWithValidSession",
-      });
+      const createdUser = await orchestrator.createUser({ username: "UserWithValidSession" });
 
       const sessionObject = await orchestrator.createSession(createdUser.id);
 
@@ -39,6 +38,21 @@ describe("GET /api/v1/user", () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const renewedSessionObject = await session.findOneValidByToken(sessionObject.token);
+
+      expect(renewedSessionObject.expires_at > sessionObject.expires_at).toEqual(true);
+      expect(renewedSessionObject.updated_at > sessionObject.updated_at).toEqual(true);
+
+      const parsedSetCookie = setCookieParser(response, { map: true });
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: "session_id",
+        value: sessionObject.token,
+        maxAge: session.EXPIRATION_IN_MILLSECONDS / 1000,
+        path: "/",
+        httpOnly: true,
+      });
     });
 
     test("With nonexistent session", async () => {
@@ -68,9 +82,7 @@ describe("GET /api/v1/user", () => {
         now: new Date(Date.now() - session.EXPIRATION_IN_MILLSECONDS),
       });
 
-      const createdUser = await orchestrator.createUser({
-        username: "UserWithExpiredSession",
-      });
+      const createdUser = await orchestrator.createUser({ username: "UserWithExpiredSession" });
 
       const sessionObject = await orchestrator.createSession(createdUser.id);
 
@@ -92,6 +104,42 @@ describe("GET /api/v1/user", () => {
         action: "Verifique se este usuário está logado e tente novamente.",
         status_code: 401,
       });
+    });
+
+    test("With session about to expire", async () => {
+      jest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLSECONDS + 60000),
+      });
+
+      const createdUser = await orchestrator.createUser({ username: "UserWithEndLifeSession" });
+
+      const sessionObject = await orchestrator.createSession(createdUser.id);
+
+      jest.useRealTimers();
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: createdUser.id,
+        username: "UserWithEndLifeSession",
+        email: createdUser.email,
+        password: createdUser.password,
+        created_at: createdUser.created_at.toISOString(),
+        updated_at: createdUser.updated_at.toISOString(),
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+      expect(sessionObject.expires_at > Date.now()).toEqual(true);
     });
   });
 });
